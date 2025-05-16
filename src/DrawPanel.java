4c14605257bfe5ee1aa0f7266e3a6b85119ca526
BT_2S14_06 <danielk356@nycstudents.net>
2025-05-16 11:00:07 -0400

    I allowed the player to move left and right.

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,39 +1,42 @@
 import java.awt.*;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.awt.Point;
 import java.awt.image.ImageObserver;
 
-public class DrawPanel extends JPanel implements KeyListener, MouseListener {
+public class DrawPanel extends JPanel implements KeyListener
+{
     private Maze mazes;
     private Player player;
 
-    public DrawPanel() {
-        this.addMouseListener(this);
+    public DrawPanel()
+    {
+        setFocusable(true);
         this.addKeyListener(this);
         this.setBackground(Color.GRAY);
         mazes = new Maze();
         player = new Player();
     }
 
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g)
+    {
         super.paintComponent(g);
         int x = 20;
         int y = 20;
         Graphics2D g2 = (Graphics2D) g;
 
-        for (int r = 0; r < 15; r++) {
+        for (int r = 0; r < 15; r++)
+        {
             for (int c = 0; c < 20; c++) {
                 g.drawRect(x, y, 40, 40);
-                if (mazes.getMaze1()[r][c] == 1) {
+                if (mazes.getMaze1()[r][c] == 1)
+                {
                     g.fillRect(x, y, 40, 40);
                 }
-                if (r == player.getYCoordinate() && c == player.getXCoordinate()) {
+                if (r == player.getYCoordinate() && c == player.getXCoordinate())
+                {
                     g.drawImage(player.getImage(), x, y, 40, 40, null);
                 }
                 x += 50;
@@ -41,34 +44,6 @@ public class DrawPanel extends JPanel implements KeyListener, MouseListener {
             x = 20;
             y += 50;
         }
-
-
-    }
-
-
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
 
     }
 
@@ -78,8 +53,24 @@ public class DrawPanel extends JPanel implements KeyListener, MouseListener {
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(KeyEvent e)
+    {
+        if (e.getKeyCode() == KeyEvent.VK_A)
+        {
+            if (player.getXCoordinate() != 0 && mazes.getMaze1()[player.getYCoordinate()][player.getXCoordinate() - 1] != 1)
+            {
+                player.setXCoordinate(player.getXCoordinate() - 1);
 
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D)
+        {
+            if (player.getXCoordinate() != mazes.getMaze1()[0].length - 1 && mazes.getMaze1()[player.getYCoordinate()][player.getXCoordinate() + 1] != 1)
+            {
+                player.setXCoordinate(player.getXCoordinate() + 1);
+
+            }
+        }
     }
 
     @Override
